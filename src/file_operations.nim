3239d2441019b4b757cc89f7d9930328d7beaf14
Osamikoyo <v.pylayev@mail.ru>
2025-04-16 14:21:22 +0500

    update file_operations

--- src/file_operations.nim
+++ src/file_operations.nim
@@ -1,4 +1,4 @@
-import os
+import os, osproc
 
 const EXAMPLE_TEMPLATE_DATA = """
 project = "coni"
@@ -16,7 +16,7 @@ path = "internal/server/server.go"
 data = "package server\n\ntype Server struct{\nserver *http.Server\n}"
 """
 
-const HOME_DIR = ".larny-templates"
+const HOME_DIR = ".templates"
 
 proc createWith*(path, data: string) =
   let filepath = path & ".toml"
@@ -47,6 +47,27 @@ proc createDefault*(name : string) =
 
 proc moveTempl*(path : string) =
   try:
-    moveFile(path, HOME_DIR)
+    let homeDir = getHomeDir()
+    let destDir = homeDir / HOME_DIR 
+
+    createDir(destDir)
+    
+    let filename = extractFilename(path)
+    
+    let destPath = destDir / filename
+    
+    if not fileExists(path):
+      raise newException(IOError, "Source file does not exist: " & path)
+    
+    if fileExists(destPath):
+      raise newException(IOError, "Destination file already exists: " & destPath)
+    
+    moveFile(path, destPath)
+    echo "File moved successfully to: ", destPath
+    
+  except OSError as e:
+    echo "OS error while moving file: ", e.msg
+  except IOError as e:
+    echo "File operation error: ", e.msg
   except:
-    echo "error move file"
+    echo "Unknown error: ", getCurrentExceptionMsg()
